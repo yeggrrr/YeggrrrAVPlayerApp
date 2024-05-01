ada21cf96b68b3d76d7911e5bfc4465de68d5e47
yeggrrr <yjkim981103@gmail.com>
2024-05-01 15:29:47 +0900

    [Add] infoplist 항목 추가 & 불필요한 파일 삭제로 인한 전체 파일 업데이트

---
+++ YeggrrrAVPlayerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YeggrrrAVPlayerApp
+//
+//  Created by YJ on 5/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
